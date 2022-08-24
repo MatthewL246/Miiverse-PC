9fb17d1561c762e7aa4d99b421f3622da98a90d0
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2022-08-23 22:39:01 -0400

    Implement full support for selecting custom screenshots
    This currently uses a file input injected into the webpage that  sets the image data to be returned by wiiuMainApplication.getScreenShot(). It should be possible to use the image select dropdown directly.

--- js/portal.js
+++ js/portal.js
@@ -1,18 +1,73 @@
-﻿window.wiiuMainApplication = {
+﻿let currentImageData;
+
+window.wiiuMainApplication = {
     getScreenShot: function (screen) {
         console.log(`Getting screenshot: ${screen}`);
 
-        var screenshot = null;
-        if (screen === true) {
-            screenshot =
-                "iVBORw0KGgoAAAANSUhEUgAAAEAAAABAAgMAAADXB5lNAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAFiUAABYlAUlSJPAAAAAJUExURQCi6H9/f+0cJJKgY/4AAACsSURBVDjL7dO7DQMhEEXRiyUS5zThKiiBAPrZUhxaU+UGvOGzGzpx4EmQrsTRCAnaZdBZ0yXkcAmwhwppCwXiFjKELYAjiHgcQhDxfAtBxOsjBBFmQhBhJgQRZkIQYSYEJxzBCUdwwhGccIRBCGEQQhiEEAYhhEl0hEl0hEl0hEl0hIUwg8ZCmB0kCsw9IFJgrg6RvIdwD7crrbXqIY0n/IefCJovQvOw/v0xJyKGX/o6cyPrAAAAAElFTkSuQmCC";
-        } else if (screen === false) {
-            screenshot =
-                "iVBORw0KGgoAAAANSUhEUgAAAEAAAABAAgMAAADXB5lNAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAFiUAABYlAUlSJPAAAAAJUExURQCi6H9/f+0cJJKgY/4AAADFSURBVDjL7dO7DcQgEIThkUNKcT9DQAlU4SbInSDBVHkBu+BHeMElt5k/aX9ZKwE+BiQpG4foUL4BAAD+8DuIkhwkEekOFfkO/Q2vlaQTc4IqotqCXQX0hbFEMOvw702d4CUSVAleIrsKwUtEIsEV2dRJcEWCKgmuyK4y7rF+3Q5kkU3dwCJBdZ6wzYQ/j5kYkHV4YkDS6YkBUc0TAyh5wiDrsIRB0mkJg6hmCQP6I52QZQmHJEs4RFnCgZ6YkPsDYnnAnA/9O0c8rY2eFAAAAABJRU5ErkJggg==";
-        } else {
-            console.error(`Unexpected value of screen: ${screen}`);
+        if (!currentImageData) {
+            console.warn(
+                "Current image data is undefined, so the image will be broken!"
+            );
         }
 
-        return screenshot;
+        return currentImageData;
     },
 };
+
+function readFile(input) {
+    return new Promise((resolve, reject) => {
+        const file = input.files[0];
+        if (!file) {
+            reject("No file was selected.");
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = (error) => {
+            const errorMessage = `Error loading file ${file}: ${error}`;
+            reject(errorMessage);
+        };
+    });
+}
+if (document.readyState !== "loading") {
+    initInputs();
+} else {
+    document.addEventListener("DOMContentLoaded", function () {
+        initInputs();
+    });
+}
+
+function initInputs() {
+    const hiddenImageInput = document.createElement("input");
+    hiddenImageInput.type = "file";
+    hiddenImageInput.accept = "image/png";
+
+    hiddenImageInput.onchange = async () => {
+        try {
+            let imageData = await readFile(hiddenImageInput);
+            currentImageData = imageData.replace("data:image/png;base64,", "");
+        } catch (ex) {
+            console.error(ex);
+        }
+
+        // From juxt.js: sets the source of the screenshot preview images
+        document.getElementById("post-top-screen-preview").src =
+            "data:image/png;base64," +
+            window.wiiuMainApplication.getScreenShot(true);
+        document.getElementById("post-bottom-screen-preview").src =
+            "data:image/png;base64," +
+            window.wiiuMainApplication.getScreenShot(false);
+    };
+
+    const loadImageButton = document.createElement("button");
+    loadImageButton.innerHTML = "Select a screenshot to post";
+    loadImageButton.style = "font-size: 24px;";
+    loadImageButton.onclick = () => {
+        hiddenImageInput.click();
+    };
+    document.body.appendChild(loadImageButton);
+}
